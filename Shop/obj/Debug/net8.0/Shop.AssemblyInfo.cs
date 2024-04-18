7dc4602261beb606301121a61c38fd00733d5780
P1SKASS <vt232_svp@student.ztu.edu.ua>
2024-04-18 17:55:23 +0200

    немного изменил header

--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9946d83243580720d075f42d4d8b7d7da1e5542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe6366fd2f6c099a82230e0ec5e0566d4200679")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
